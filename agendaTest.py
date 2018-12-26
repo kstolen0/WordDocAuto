c275930502b379098adcd2ce99a2609c849c0884
Kristian Stolen <kstolen0@our.ecu.edu.au>
2018-12-26 13:50:49 +0800

    Added AddItem functionality
    Allows user to add agenda items to dictionary.
    Doesn't upload to document but saves data between sessions.

--- agendaTest.py
+++ agendaTest.py
@@ -1,12 +1,125 @@
 from mailmerge import MailMerge
 import datetime
-import time 
+import time
 import os
 import json
 
+# main loop
+def main():
+
+    # load config file
+    try:
+        config = LoadFile('config.json')
+    except Exception as e:
+        print("config file error?" + e)
+        input("Seek help!")
+        return
+    config = config['DEFAULT']
+
+    # initialise values
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    vals = { 'theDate' : '0', 'theTime' : '0', 'theDuration' : '0', 'kdp' : [] }
+    usrInput = ''
+
+
+
+    # enter main loop
+    while(usrInput != 'e'):
+        print('1) Input meeting date')
+        print('2) Input meeting time')
+        print('3) Input meeting duration')
+        print('4) Display meeting details')
+        print('5) Load meeting details')
+        print('6) Add agenda item')
+        print('c) Create doc')
+        print('e) Exit')
+
+        usrInput = input('--> ')
+
+        if(usrInput == '1'):
+            vals['theDate'] = dateInput()
+            input('success!')
+
+        elif(usrInput == '2'):
+            vals['theTime'] = timeInput()
+            input('success!')
+
+        elif(usrInput == '3'):  # returns the time added to vals['theTime'] in 24 hour format
+            vals['theDuration'] = addTime('Enter the duration of the meeting in minutes: ',vals['theTime'])
+            input('success!')
+
+        elif(usrInput == '4'): # print all fields
+            print('Meeting date: ' + vals['theDate'])
+            print('Meeting start time: ' + vals['theTime'])
+            print('Meeting end time: ' + vals['theDuration'])
+            if(len(vals['kdp']) > 0):
+                configItem = config['key_decisions']
+                print('\n/\\_________________________________________/\\')
+                print('|-- ID | TOPIC | PRESENTER | ACTION | TIME --|')
+                print('L------I-------I-----------I--------I--------J\n')
+                for n in vals['kdp']:
+                    print('r-----------------------')
+                    print('| ' +
+                        n[configItem['id']] + ' + ' +
+                        n[configItem['topic']] + ' + ' +
+                        n[configItem['presenter']] + ' + ' +
+                        n[configItem['action']] + ' + ' +
+                        n[configItem['time']])
+                    print('L_______________________')
+
+            input('...')
+
+        elif(usrInput == '5'):
+            if(UserConfirmed()): # confirm we want to overwrite
+                try:
+                    vals = LoadFile('save.json') # if a failure occurs, don't overwrite the current vals
+                except Exception as e: # need more explicit exceptions?
+                    print("failed: " + str(e))
+                    print(vals)
+
+        elif(usrInput == '6'):
+            item = AddItem(config, vals['theTime'] ,len(vals['kdp']))
+            vals['kdp'].append(item)
+            print(vals)
+
+        elif(usrInput == 'c'):
+            MakeDoc(vals)
+            input('success!')
+
+        elif(usrInput == 'e'):
+            pass
+
+        else:
+            print("Invalid input")
+
+
+        os.system('cls')
+
+
+    # save vals to json file
+    saveFile = vals
+
+    with open('save.json','w') as outfile:
+        json.dump(saveFile,outfile)
+
+def AddItem(config, theTime,listLen):
+    item = {}
+    configItem = config['key_decisions']
+
+    item[configItem['id']] = configItem['code'] + '.' + str(listLen + 1)
+    item[configItem['topic']] = input('enter the name of the item: ')
+    item[configItem['presenter']] = input('enter the initials of the presenter: ')
+    item[configItem['time']] = xDigitInput('Enter the duration of the item in minutes: ',1,3)
+    item[configItem['action']] = 'TODO'
+
+    return item
+
+
+
+# function to input the date
 def dateInput():
     valid = 0
-    
+
     while (valid == 0):
         dayInput = xDigitInput("enter the date as <dd>: ", 2, 2)
         monInput = xDigitInput("enter the month as <mm>: ", 2, 2)
@@ -16,11 +129,12 @@ def dateInput():
         try:
             datetime.datetime.strptime(date,'%d/%m/%Y')
             valid += 1
-        except ValueError:            
+        except ValueError:
             print("Invalid date. Try again, buddy.")
-    
+
     return date
 
+# function to input the time in 24 hour format
 def timeInput():
     valid = 0
 
@@ -33,21 +147,22 @@ def timeInput():
 
     if(int(usrInput) == 2400):
         usrInput = '0000'
-        
+
     return usrInput
 
+# function to convert 24 hour time to 12 hour time
 def to12hour(theTime):
     t = time.strptime(theTime,'%H%M')
     time12hour = time.strftime("%I:%M %p",t)
 
     return time12hour
 
-# 24 hour time 
+# function to add time (in minutes) to 24 hour time
 def addTime(query, theTime):
     valid = 0
     hour = int(theTime[:2])
     mins = int(theTime[2:])
-    
+
     while (valid == 0):
         usrInput = int(xDigitInput(query,1,3))
         if (usrInput > 720):
@@ -62,40 +177,41 @@ def addTime(query, theTime):
         mins -= 60
     if (mins < 10):
         mins = '0' + str(mins)
-        
+
     if (hour > 23):
         hour -= 24
     if (len(str(hour)) < 4):
-        hour = '0' + str(hour) 
-        
+        hour = '0' + str(hour)
+
 
     return str(hour) + str(mins)
-        
-        
 
-# positive whole numbers 
+# positive whole numbers within a range of factors (length)
 def xDigitInput(inputString,minLen,maxLen):
     valid = 0
 
     while (valid == 0):
         usrInput = input(inputString)
-        if(not usrInput.isdigit() or (len(usrInput) < minLen) or (len(usrInput) > maxLen)):
+        if(not usrInput.isdigit() or
+            (len(usrInput) < minLen) or
+            (len(usrInput) > maxLen)):
             print("Invalid entry")
         else:
             valid += 1
-            
+
     return usrInput
 
+# function to create a new word doc based on a template, filling in the mergeFields within the source file
 def MakeDoc(vals):
 
     try:
         agendaFile = dir_path + "\\" + config['docFile']
-        
+
         print(agendaFile)
         template = {}
         template[config['meeting_date']] = vals['theDate']
         template[config['meeting_time']] = to12hour(vals['theTime'])
-        template[config['meeting_duration']] = to12hour(vals['theDuration']) 
+        template[config['meeting_duration']] = to12hour(vals['theDuration'])
 
         print(template)
 
@@ -109,82 +225,21 @@ def MakeDoc(vals):
 
 def LoadFile(fileName):
 
-        
     with open(fileName,'r') as f:
         vals = json.load(f)
 
-    return vals     
+    return vals
 
 def UserConfirmed():
     x = ''
-    while not (x == 'Y' or x == 'N'): 
+    while not (x == 'Y' or x == 'N'):
         x = input('Overwrite current data with saved data (Y/N): ').upper()
 
-    return (x == 'Y') 
-    
-def main():
+    return (x == 'Y')
 
-    config = LoadFile('config.json') 
-    config = config['DEFAULT']
-    
-    dir_path = os.path.dirname(os.path.realpath(__file__))
 
-    vals = { 'theDate' : '0', 'theTime' : '0', 'theDuration' : '0' }
 
-    
-    usrInput = ''
-    
-    while(usrInput != 'e'):
-        print('1) Input meeting date')
-        print('2) Input meeting time')
-        print('3) Input meeting duration')
-        print('4) Display meeting details') 
-        print('5) Load meeting details')
-        print('c) Create doc') 
-        print('e) Exit')
-        
-        usrInput = input('--> ')   
-
-        if(usrInput == '1'):
-            vals['theDate'] = dateInput()
-            input('success!')
-            
-        elif(usrInput == '2'):
-            vals['theTime'] = timeInput()
-            input('success!')
-            
-        elif(usrInput == '3'):
-            vals['theDuration'] = addTime('Enter the duration of the meeting in minutes: ',vals['theTime'])
-            input('success!')
-            
-        elif(usrInput == '4'):
-            print('Meeting date: ' + vals['theDate'])
-            print('Meeting start time: ' + vals['theTime'])
-            print('Meeting end time: ' + vals['theDuration'])
-            input('...')
-            
-        elif(usrInput == '5'):            
-            if(UserConfirmed()): 
-                vals = LoadFile('save.json')
-                
-        elif(usrInput == 'c'):
-            MakeDoc(vals)
-            input('success!')
-            
-        elif(usrInput == 'e'):
-            pass
-        
-        else:
-            print("Invalid input")
-
-        
-        os.system('cls')
-
-    
-    # save data to json file
-    saveFile = vals
-
-    with open('save.json','w') as outfile:
-        json.dump(saveFile,outfile) 
-
-main()
+try:
+    main()
+except Exception as e:
+    input("over:" + str(e))
